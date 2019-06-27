057fb7045fd1b8af30e08b8765691f63cfa96ba1
mishravivek43 <mishravivek43@gmail.com>
2019-06-27 20:48:45 +0530

    Update Song.js
    Adding additional comments

--- song.js
+++ song.js
@@ -1,4 +1,5 @@
 /*
+Author => Vivek Mishra
 ****Starting of Song.js
 name,genre,music,lyrics,album => String
 artists,singers =>Array
@@ -7,50 +8,45 @@ isClassical=>Boolean
 //Inialising Function as a constructor
 
 function songConstructor(name,artists,genre,music,lyrics,isClassical,album,singers){
-        this.name = name;
-        this.artists = artists;
-        this.genre = genre;
-        this.music = music;
-        this.lyrics = lyrics;
-        this.isClassical = isClassical;
-        this.album = album;
-        this.singers = singers;
-        
-        //Function with Conditional Statement
-        this.showType = function(){
-                if(this.isClassical){
-                        console.log('this is classical music');
-                }else{
-                         console.log('this is modern music');
-                }
-        }
-        
-        //Function with for lopp showing all artist
-        this.sayArtistsNames = function() {
-                for(var i = 0;i<this.artists.length;i++){
-                        console.log('Artist '+parseInt(i+1)+': '+this.artists[i]);
-                }
-
-        }
-        //Function with for loop showing all singers
-        this.saySingersNames = function() {
+	this.name = name;
+	this.artists = artists;
+	this.genre = genre;
+	this.music = music;
+	this.lyrics = lyrics;
+	this.isClassical = isClassical;
+	this.album = album;
+	this.singers = singers;
+	this.showType = function(){
+		if(this.isClassical){
+			console.log('this is classical music');
+		}else{
+			 console.log('this is modern music');
+		}
+	}
+	this.sayArtistsNames = function() {
+		for(var i = 0;i<this.artists.length;i++){
+			console.log('Artist '+parseInt(i+1)+': '+this.artists[i]);
+		}
+	
+	}
+	this.saySingersNames = function() {
                 for(var i = 0;i<this.singers.length;i++){
                         console.log('Singer '+parseInt(i)+': '+this.singers[i]);
                 }
 
         }
 }
-//Initialsing arrays
+//Initialsing array
 var singers = ['A R Rahman', 'Sukhwinder Singh', 'Tanvi', 'Mahalakshmi Iyer', 'Vijay Prakash'];
 var artists = ['Anil Kapoor', 'Irrfan Khan', 'Dev Patel', 'Freida Pinto'];
 
-//Initialising Object
 var songOne = new songConstructor('Jai Ho',artists,'Bolywood','A R Rahman','Gulzaar',false,'Slumdog Millionaire');
 
-//Calling Object Functions
-songOne.showType();
+songOne.showType(); 
 songOne.sayArtistsNames();
-
-//Integer
+//Integer Initialization
 var lengthOfArtists = songOne.artists.length;
 console.log('Total no of artists: '+lengthOfArtists);
+
+//****-----End OF File -----****
+
